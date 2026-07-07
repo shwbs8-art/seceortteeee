69ac79c69370f640c5bcbb1cbc17c5de988c1705
shwbs8-art <shwbs208@gmail.com>
2026-07-07 06:04:01 +0300

    Delete events directory

--- events/inviteCreate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'inviteCreate',
-  async execute(invite) {
-    const client = invite.client;
-    if (!client.inviteCache.has(invite.guild.id)) client.inviteCache.set(invite.guild.id, new Map());
-    client.inviteCache.get(invite.guild.id).set(invite.code, invite.uses || 0);
-  },
-};
--- events/inviteDelete.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'inviteDelete',
-  async execute(invite) {
-    const client = invite.client;
-    const guildMap = client.inviteCache.get(invite.guild.id);
-    if (guildMap) guildMap.delete(invite.code);
-  },
-};
